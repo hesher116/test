13738856933227d9f4234bdb8bd92a26a6ba28e7
hesher116 <georg116@ukr.net>
2024-05-14 00:23:33 +0200

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hesher116/test
+
+go 1.22.0
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	var obj, num float32
+	var err error
+	fmt.Print("Вкажіть вартість товару: ")
+	_, err = fmt.Scanln(&obj)
+	if err != nil {
+		fmt.Println("Вкажіть вартість товару!")
+		return
+	}
+	fmt.Print("Вкажіть кількість одиниць товару: ")
+	_, err = fmt.Scanln(&num)
+	if err != nil {
+		fmt.Println("Вкажіть кількість одиниць товару!")
+		return
+	}
+
+	fmt.Printf("Загальна вартість покупки без комісії= %.2f\n", obj*num)
+
+	//switch {
+	//case year > age:
+	//	fmt.Printf("Вам %d%s\n", year-age, " років(рік)")
+	//case age > year:
+	//	fmt.Println("Введений рік народження більше ніж поточний рік")
+	//default:
+	//	fmt.Println("Помилка вводу!")
+	//}
+}
